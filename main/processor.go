48284d4e3c229096118fd3f6c7a7ed5e668e2c9f
swayamraina <swayam.raina@swiggy.in>
2019-12-06 23:31:26 +0530

    added main processor

---
+++ main/processor.go
@@ -0,0 +1,24 @@
+
+// @author 	swayam.raina
+// @dated 	06.12.2019
+
+
+package main
+
+
+import (
+	"lua-scanner/main/cache"
+	"lua-scanner/main/config"
+	"lua-scanner/main/scanner"
+)
+
+
+//
+//
+func process (config config.Config) cache.Cache {
+	scanner := scanner.Scanner {
+		 config,
+	}
+	scripts := scanner.Scan()
+	return scanner.LoadInCache(scripts)
+}
