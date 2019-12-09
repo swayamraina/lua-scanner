306525b0b00c0a0247a5f82d54156ce8b41d14a1
swayamraina <swayamraina@gmail.com>
2019-12-09 20:51:13 +0530

    added cache instance

--- main/cache/cache.go
+++ main/cache/cache.go
@@ -6,6 +6,12 @@
 package cache
 
 
+// This global variable stores all the
+// scanned data and is responsible to
+// serve cache hits
+//
+var LuaScannerCache Cache
+
 //
 //
 type Cache map[string]string
@@ -13,13 +19,13 @@ type Cache map[string]string
 
 //
 //
-func (cache Cache) Get (key string) string {
-	return cache[key]
+func (cache *Cache) Get (key string) string {
+	return (*cache)[key]
 }
 
 
 //
 //
-func (cache Cache) Set (key string, value string) {
-	cache[key] = value
+func (cache *Cache) Set (key string, value string) {
+	(*cache)[key] = value
 }
