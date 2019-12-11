f636a01a0096856721905372e66b3247463e43b3
swayamraina <swayamraina@gmail.com>
2019-12-12 00:05:01 +0530

    added docs

--- main/processor/processor.go
+++ main/processor/processor.go
@@ -1,6 +1,13 @@
+/*
 
-// @author 	swayam.raina
-// @dated 	06.12.2019
+	This file declares a public method which is
+	used by the user application to pitch in with the
+	scan config to get the resource cache.
+
+	@author 	swayam.raina
+	@dated 		06.12.2019
+
+ */
 
 
 package processor
@@ -13,8 +20,17 @@ import (
 )
 
 
-//
-//
+
+/*
+	This function is the 'open to world' function for this
+	lib.
+	Application needs to pass in the scanner config which
+	is used by the scanner to create a cache object which
+	stores the resource contents by its name.
+
+	config:			the scanner config
+	return: 		the cache object storing the resource data
+ */
 func Process (config config.Config) cache.Cache {
 	scanner := scanner.Scanner {
 		 Config: config,
