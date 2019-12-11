fc856131ecc206d6145570f2b1924bf492e5287e
swayamraina <swayamraina@gmail.com>
2019-12-11 23:50:20 +0530

    added docs

--- main/config/config.go
+++ main/config/config.go
@@ -1,24 +1,39 @@
+/*
 
-// @author 	swayam.raina
-// @dated 	05.12.2019
+	This file is the defines the config for the
+	'lua-scanner'.
+
+	@author 	swayam.raina
+	@dated 		05.12.2019
+
+ */
 
 
 package config
 
 
-// This structure holds the config for the scanner
-//
+
+/*
+	This data structure stores the config for the
+	resource scanner.
+	The config object is to provided by the application
+	owners into the 'processor.Process()' function call
+	for the scanner to do the resource discovery.
+ */
 type Config struct {
 
-	// this property is used by the scanner to
-	// look for all the lua scripts in the
-	// project for discovery
-	//
+	/*
+		This property is used by the scanner to look
+		for all the lua scripts in the provided directory
+		for discovery.
+	 */
 	InternalPaths []string
 
-	// this property acts as a switch for internal
-	// lua script discovery scan
-	//
+
+	/*
+		This property acts as a switch for internal
+		lua script discovery scan
+	 */
 	InternalScan bool
 
 }
